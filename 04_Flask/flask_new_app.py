d9d0321169481901cd48334e66b36ce4807c7b4d
Hanseok <hasuikhs@gmail.com>
2021-06-03 21:54:50 +0900

    210603|Update flask response after processing

--- 04_Flask/flask_new_app.py
+++ 04_Flask/flask_new_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+import threading, time
 
 app = Flask(__name__)
 
@@ -7,6 +8,14 @@ app = Flask(__name__)
 #     if request.remote_addr != '<white list ip>':
 #         abort(403)
 
+sem = threading.Semaphore(value=10)
+sem.acquire()
+
+def response_after(second):
+    time.sleep(second)
+    
+    return 'test'
+
 @app.route('/')
 def index():
     return 'hello'
@@ -14,7 +23,12 @@ def index():
 @app.route('/scrap/<string:target>/<string:user>', methods=['GET'])
 def scrap(target, user):
     
+    
     avail_scrap_list = ['avail1', 'avail2']
+    
+    print(threading.active_count())
+    th = threading.Thread(target=response_after, args=(10, ))
+    th.start()
     
     # if (request.headers.get('<header에 담기는 api token 등>') == None):
     #     abort(401)
@@ -44,4 +58,4 @@ def page_not_found(error):
 
 
 if __name__ =='__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
