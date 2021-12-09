463ca508ccdb779d157db2ad777e2fc3d918cb2e
Hanseok <hasuikhs@gmail.com>
2021-12-09 17:04:04 +0900

    211209|Update test with jest

---
+++ 06_JavaScript/06_Typescript/react_express/server/test/dataManager.test.ts
@@ -0,0 +1,40 @@
+import DataManager from '../src/service/classes/dataManager';
+
+describe("Test DataManger methods.", () => {
+
+  // account
+  // {"id":"test_id_1","password":"test_pw_1","group":["group_1"]}
+
+  // doc
+  // {"title": "title_1", "url": "url_1"}
+
+  // server
+  // {"name":"test_server_1","key":"server_key_1","cpu":4,"memory":16,"disk":50,"group":"group_1","active":true,"publicIp":"123.123.123.123"}
+
+  // user
+  // {"name":"test_1"}
+
+  const dataManager = new DataManager();
+
+  it('Insert test', async () => {
+
+    let account = {
+      "id": "test_id_1", "password": "test_pw_1", "group": ["group_1"]
+    }
+
+    await expect(dataManager.insert('account', account)).resolves.toBe(1);
+  });
+
+  // it('Select test', () => {
+
+  // });
+
+  // it('Update test', () => {
+
+  // });
+
+  // it('Delete test', () => {
+
+  // });
+
+});
