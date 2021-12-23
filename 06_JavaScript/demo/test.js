cd69d57d4f227ebae5ef0c53509b652510ed5a1a
Hanseok <hasuikhs@gmail.com>
2021-12-23 22:16:41 +0900

    211223|Add dynamic import js module

---
+++ 06_JavaScript/demo/test.js
@@ -0,0 +1,13 @@
+function test1() {
+  console.log('test1');
+}
+
+function test2() {
+  console.log('test2')
+}
+
+export default function test3() {
+  console.log('test3');
+}
+
+export {test1, test2};
