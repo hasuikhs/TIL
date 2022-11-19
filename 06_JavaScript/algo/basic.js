326563f9b2539d2c761c7b9f3adf068a9242014f
hanseok kim <hasuikhs@gmail.com>
2022-11-19 23:08:04 +0900

    221119|Update

--- 06_JavaScript/algo/basic.js
+++ 06_JavaScript/algo/basic.js
@@ -2,8 +2,7 @@
 // 유클리드 호제법
 // a, b (a > b) a를 b로 나눈 나머지가 r일때 GCD(a, b) = GCD(b, r)를 반복하고 r이 0이면 그때 b가 최대공약수
 function getGCD(a, b) {
-  if (b === 0) return a;
-  else return getGCD(b, a % b);
+  return b === 0 ? a : getGCD(b, a % b)
 }
 
 // 최소 공배수
